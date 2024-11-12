c14a317aaf2f3eae0aff21bfb68889847e7e9373
Amir H Nazeri <anazeri@clemson.edu>
2024-11-12 18:16:16 -0500

    main.py edited v1.0

--- main.py
+++ main.py
@@ -1,26 +1,61 @@
-from data_scraper import flattener
+import sys
+custom_path = "/path/to/your/files"
+sys.path.append(custom_path)
+
+import data_scraper
+import RAG
+import openai_chat
+from data_scraper import flattener, load_urls_from_file
 from RAG import generate_embeddings, create_faiss_index, query_table_data
 from openai_chat import chatbot_response, create_prompt
+import os
+
 
 # URLs to scrape data from
-urls = [
-    "https://www.ford.com/suvs-crossovers/edge/models/edge-se/",
-    "https://www.ford.com/suvs/explorer/2024/models/explorer-st/",
-    # Add more URLs as needed
-]
+# urls = data_scraper.load_urls_from_file(custom_path+"data/urls.csv")
+urls = ["https://www.ford.com/suvs-crossovers/edge/models/edge-se/",
+        "https://www.ford.com/suvs/explorer/2024/models/explorer-st/",
+        "https://www.ford.com/suvs/mach-e/models/mach-e-select/",
+        "https://www.ford.com/suvs/expedition/models/expedition-xl-stx/",
+        "https://www.ford.com/cars/mustang/2024/models/gt-fastback/",
+        "https://www.ford.com/suvs-crossovers/escape/2024/models/escape-st-line/",
+        "https://www.ford.com/suvs/bronco-sport/models/bronco-sport-big-bend/",
+        "https://www.ford.com/suvs/bronco/models/bronco-badlands/"]
 
-# Step 1: Scrape and parse HTML tables
-all_rows = flattener(urls)
 
-# Step 2: Generate embeddings and create FAISS index
-embeddings = generate_embeddings(all_rows)
-index = create_faiss_index(embeddings)
+class AI_chatbot():
 
-# Step 3: Define query and retrieve relevant data
+    def __init__(self, urls, query):
+        self.urls = urls
+        self.query = query
+
+    def get_web_data(self):
+        # Scrape and parse HTML tables
+        return flattener(self.urls)
+
+    def get_rag_indices(self):
+        # Generate embeddings
+        embeddings = generate_embeddings(self.get_web_data())
+        # create FAISS index
+        return create_faiss_index(embeddings)
+
+    def get_relevant_data(self):
+        # Get retrieveal relevant data
+        return query_table_data(self.query, self.get_rag_indices(), self.get_web_data())
+
+    def generate_response(self, no_rag = True):
+        # Generate response using OpenAI API
+
+        prompt = create_prompt(self.get_relevant_data(), self.query)
+        if no_rag:
+            prompt = create_prompt("No extra information. asnwer based on your own knowledge", self.query)
+        return chatbot_response(prompt)
+    
+
+
+
+"""
 query = "Explain the engine of Ford Expedition 2024."
-relevant_data = query_table_data(query, index, all_rows)
-
-# Step 4: Generate response using OpenAI API
-prompt = create_prompt(relevant_data, query)
-response = chatbot_response(prompt)
-print(response)
+AI_chatbot_run = AI_chatbot(urls, query)
+AI_chatbot_run.generate_response()
+"""
